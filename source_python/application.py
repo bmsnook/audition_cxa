e758ad77202e7b6fc07d9581f9edd9b933ada842
Brian Snook <1421890+bmsnook@users.noreply.github.com>
2018-07-26 18:21:53 -0400

    updated application.py file

--- source_python/application.py
+++ source_python/application.py
@@ -36,7 +36,7 @@ welcome = """
     or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
   -->
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <title>Welcome</title>
+  <title>Bienvenue</title>
   <style>
   body {
     color: #ffffff;
@@ -110,8 +110,8 @@ welcome = """
 </head>
 <body id="sample">
   <div class="textColumn">
-    <h1>Congratulations</h1>
-    <p>Your first AWS Elastic Beanstalk Python Application is now running on your own dedicated environment in the AWS Cloud</p>
+    <h1>Bonjour, tout le monde!</h1>
+    <p>My second AWS Elastic Beanstalk Python Application is now running on my own dedicated environment in the AWS Cloud</p>
   </div>
   
   <div class="linksColumn"> 
